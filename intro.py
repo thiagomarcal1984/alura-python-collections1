dffff4b7dbbfd18344e777852ebb1c3018d2b0f3
Thiago Marçal <thiagomarcal1984@gmail.com>
2023-11-04 07:42:48 -0300

    Mais operações em listas e list comprehension

--- intro.py
+++ intro.py
@@ -14,28 +14,37 @@ idades = [39, 30, 27, 18]
 print()
 print('O tipo da variável "idades" é "list":')
 print(type(idades)) 
+# <class 'list'>
 
 print()
 print('O comprimento da lista é de 4 elementos:')
 print(len(idades)) 
+# 4
 
 print()
 print('Impressão das idades armazenadas em uma lista')
 for idade in idades:
     print(idade)
+# 39
+# 30
+# 27
+# 18
 
 print()
 idades.append(15)
 print(f'Acrescentei o número {idades[-1]} à lista de idades: ')
 print(idades)
+# [39, 30, 27, 18, 15]
 
 print()
 removido = 30
 idades.remove(removido)
 print(f'Removi o número {removido} da lista de idades: ')
 print(idades)
+# [39, 27, 18, 15]
 
 print()
 idades.clear()
 print(f'Limpei a lista de idades:')
 print(idades)
+# []
--- mais_operacoes.py
+++ mais_operacoes.py
@@ -1,59 +1,3 @@
-# Introdução as coleções e lista
-```python
-idade1 = 39
-idade2 = 30
-idade3 = 27
-idade4 = 18
-
-print('Impressão individual (e trabalhosa) das idades: ')
-print(idade1)
-print(idade2)
-print(idade3)
-print(idade4)
-
-idades = [39, 30, 27, 18]
-
-print()
-print('O tipo da variável "idades" é "list":')
-print(type(idades)) 
-# <class 'list'>
-
-print()
-print('O comprimento da lista é de 4 elementos:')
-print(len(idades)) 
-# 4
-
-print()
-print('Impressão das idades armazenadas em uma lista')
-for idade in idades:
-    print(idade)
-# 39
-# 30
-# 27
-# 18
-
-print()
-idades.append(15)
-print(f'Acrescentei o número {idades[-1]} à lista de idades: ')
-print(idades)
-# [39, 30, 27, 18, 15]
-
-print()
-removido = 30
-idades.remove(removido)
-print(f'Removi o número {removido} da lista de idades: ')
-print(idades)
-# [39, 27, 18, 15]
-
-print()
-idades.clear()
-print(f'Limpei a lista de idades:')
-print(idades)
-# []
-```
-
-# Mais operações em listas e list comprehension
-```python
 print('Redeclarando a variável "idades"')
 idades = [39, 18, 15, 27]
 print(idades)
@@ -141,4 +85,3 @@ def proximo_ano(idade):
 print('Imprimindo apenas as idades maiores que 21 do próximo ano:')
 print([ proximo_ano(idade) for idade in idades if idade > 21])
 # [40, 28]
-```
